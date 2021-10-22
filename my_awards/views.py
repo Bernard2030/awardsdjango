e374b1c7bf9d8eac82718bba422cc7880fa9d0e4
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-23 00:38:00 +0300

    add views content

--- my_awards/views.py
+++ my_awards/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-
-from my_awards.models import My_projects
+from my_awards.models import My_projects, Profile, Comments, Rates
+from django.contrib.auth.models import User
+from .forms import CommentForm, UpdateProfileForm, ProjectFormNew
+from django.contrib import messages
+from django.contrib.auth import logout
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -33,7 +36,26 @@ def my_projects_new(request):
 
     else:
         form = ProjectFormNew()
-    return render(request, 'project.html',{'form':form})            
+    return render(request, 'project.html',{'form':form})
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+
+    if 'my_projects' in request.GET and request.GET['my_projects']:
+        search_term = request.GET.get('my_projects')
+        searched_my_projects = My_projects.search_my_projects(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'my_projects':searched_my_projects}) 
+
+    else:
+        message = 'you have not entered anything to search'
+        return render(request, 'search.html',{'message':message})
+        
+               
+
+
 
 
 
