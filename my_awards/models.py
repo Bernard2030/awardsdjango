2c0ab37e61ca2ddf18d2d95b3e25318ab31fb811
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-22 22:48:17 +0300

    add cloudinary

--- my_awards/models.py
+++ my_awards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    image = CloudinaryField('image')
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
