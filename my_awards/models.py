d8da964aabd577897f37d09c4460f2284f47a53c
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-22 23:31:59 +0300

    add more models in models file

--- my_awards/models.py
+++ my_awards/models.py
@@ -7,3 +7,80 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     image = CloudinaryField('image')
     user = models.OneToOneField(User, on_delete = models.CASCADE)
+    bio = models.TextField(max_length=1200)
+    information = models.TextField(max_length=1500)
+
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete() 
+
+
+
+
+class My_projects(models.Model):
+    image = CloudinaryField('image')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    description = models.TextField(max_length=1500)
+    links =models.URLField()
+
+    def save_my_projects(self):
+        self.save()
+
+
+    @classmethod
+    def all_my_projects(cls):
+
+        all_my_projects = cls.objects.all()
+        return all_my_projects
+
+
+    @classmethod
+    def single_my_projects(cls,id):
+        single_my_projects = cls.objects.filter(id=id)
+        return single_my_projects
+
+    @classmethod
+    def user_my_projects(cls,user):
+        user_my_projects = cls.objects.filter(user=user)
+        return user_my_projects
+
+    @classmethod
+    def search_my_projects(cls,search_term):
+        searched_my_projects = cls.objects.filter(title = search_term)
+        return searched_my_projects 
+
+
+
+class Rates(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    my_projects_id = models.ForeignKey(My_projects, on_delete=models.CASCADE)
+    userbility = models.IntegerField(default=1)
+    design = models.IntegerField(default=1)
+    content = models.IntegerField(default=1)                      
+
+
+class Comments(models.Model):
+    my_projects_id = models.ForeignKey(My_projects, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.CharField(max_length=1200)
+
+    def __str__(self):
+        return self.user
+
+
+    @classmethod
+    def get_all_comments(cls,id):
+        comments = cls.objects.filter(my_project_id = id)
+        return comments
+
+    def save_comments(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()            
