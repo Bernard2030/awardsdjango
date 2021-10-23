808cf49e5c5c4fcd9afa4b3346845a4a7bee361c
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-23 14:43:19 +0300

    add content to comment html

--- my_awards/urls.py
+++ my_awards/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import  url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    path('comment/<int:id>/',views.comment,name='comment'),
 ]
 
 if settings.DEBUG:
