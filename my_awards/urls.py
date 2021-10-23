7c9705db947a40adf88c86c51cc766eed69d26d4
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-23 15:31:11 +0300

    add search template

--- my_awards/urls.py
+++ my_awards/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     path('comment/<int:id>/',views.comment,name='comment'),
+    url(r'^search/',views.search_results,name = 'search_results'),
 ]
 
 if settings.DEBUG:
