bea90abfd39aadc4f1c512ab4482e996477d838e
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-23 11:10:56 +0300

    add comments function in views py

--- my_awards/views.py
+++ my_awards/views.py
@@ -52,8 +52,84 @@ def search_results(request):
     else:
         message = 'you have not entered anything to search'
         return render(request, 'search.html',{'message':message})
-        
-               
+
+@login_required(login_url='/accounts/login/')
+def comment(request,id):
+    id=id
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.user = request.user
+            my_projects = My_projects.objects.get(id=id)
+            comment.my_projects_id = my_projects
+            comment.save()
+            return redirect('home')
+
+        else:
+            my_projects_id = id
+            messages.information(request, 'fill all the fields')
+            return redirect ('comment',id = my_projects_id)
+
+    else:
+        id = id
+        form = CommentForm()
+        return render(request, 'comment.html',{'form':form, 'id':id})
+
+
+@login_required(login_url='/accounts/login/')
+def rates(request,id):
+    if request.method == 'POST':
+        rates = Rates.objects.filter(id = id)
+        for rates in rates:
+            if rates.user == request.user:
+                messages.information(request,'you only rate once')
+                return redirect('singleproject',id)
+
+            else:
+                messages.information(request,'Input all fields')
+                return redirect('singleproject', id)
+
+        else:
+            messages.information(request, 'Input all fields')
+            return redirect('singleproject', id) 
+
+
+
+@login_required(login_url='/accounts/login/')
+def logout_request(request):
+    """
+    The function logs out user
+    """                       
+
+    logout(request)
+    return redirect('home')
+
+
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
+
+        else:
+            form = UpdateProfileForm(request.POST,request.FILES)
+            return render(request,'update_profile.html',{'form':form})
+                    
+
+
+
+    
+
+
+
+
 
 
 
