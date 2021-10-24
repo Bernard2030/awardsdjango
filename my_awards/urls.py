db58fa3d79e707e625e9afbcfcf43cf195f405c0
Bernard Opiyo <brobernard.254@gmail.com>
2021-10-25 00:32:37 +0300

    modify singleproject templates

--- my_awards/urls.py
+++ my_awards/urls.py
@@ -11,6 +11,7 @@ urlpatterns=[
     url(r'^search/',views.search_results,name = 'search_results'),
     url(r'^singleproject/(\d+)',views.single_project,name='singleproject'),
     path('rate/<int:id>/',views.rates,name='rates'),
+     url(r'^newproject/$',views.my_projects_new,name='newproject'),
     
 ]
 
